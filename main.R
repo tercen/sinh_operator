2ac0ea318e45e00330090c8b442e81da3396cf31
lucas501 <lucas.lelann@gmail.com>
2022-07-05 15:33:09 +0000

    add tim test and populate workflows

--- main.R
+++ main.R
@@ -1,14 +1,24 @@
 library(tercen)
 library(dplyr)
 
-ctx = tercenCtx()
-scale = NULL
+#library(tim)
+#options("tercen.workflowId"= "6015a4dd34cef273755e1a1b1500427b")
+#options("tercen.stepId"= "d31241f6-173f-473a-9307-2b4b3c5c0882")
+
+ctx <- tercenCtx()
+scale <- NULL
 if (as.character(ctx$op.value('scale')) != "NULL")  scale  = as.integer(ctx$op.value('scale'))
 
-ctx  %>%
+#scale <- 5
+
+df.out<-ctx  %>%
   select(.ri, .ci, .y) %>%
   group_by(.ri, .ci) %>%
   mutate(sinh = (sinh(.y) * scale)) %>%
   select(.ri, .ci, sinh) %>%
-  ctx$addNamespace() %>%
+  ctx$addNamespace()
+
+df.out %>%
   ctx$save()
+
+#tim::build_test_data(res_table = df.out, ctx = ctx, test_name = "test1")
